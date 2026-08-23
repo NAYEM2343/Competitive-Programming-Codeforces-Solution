19370e49e77ccf7aa1b329ba591f38d1a0d0d406
NAYEM2343 <nayemsheikh822@gmail.com>
2026-08-23 23:55:45 +0600

    Added Codeforces solutions

--- src/GeorgeandAccommodation467A.java
+++ src/GeorgeandAccommodation467A.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
 public class GeorgeandAccommodation467A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rooms = 0;
+        int [][] arr = new int[n+1][3];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                arr[i][j] = sc.nextByte();
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i][1]-arr[i][0]>=2)
+            {
+                rooms++;
+            }
+        }
+        System.out.println(rooms);
+    }
 }
---
+++ src/Magnets344A.java
@@ -0,0 +1,2 @@
+public class Magnets344A {
+}
