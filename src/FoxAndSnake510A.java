e99d05934d3e520f0621b73ac1a46f2b8535250b
NAYEM2343 <nayemsheikh822@gmail.com>
2026-08-27 03:02:07 +0600

    Added

--- src/FoxAndSnake510A.java
+++ src/FoxAndSnake510A.java
@@ -7,43 +7,38 @@ public class FoxAndSnake510A {
         boolean right = true;
         for(int i=0;i<row;i++)
         {
-
-            if(i%2==1)
-            {
-
-                for(int j=0;j<colun-1;j++)
-                {
-                    if(right)
-                    {
-                        System.out.print(".");
-                        if(colun-2==j)
-                        {
-                            System.out.print("#");
-                        }
-                    }
-                    else
-                    {
-
-                        if(j-i==0)
-                        {
-                            System.out.print("#");
-                        }
-
-                    }
-                }
-                System.out.print("");
-
-            }
-            else
+            if(i%2==0)
             {
                 for(int j=0;j<colun;j++)
                 {
                     System.out.print("#");
                 }
+            }
+            else
+            {
+                if(right==true)
+                {
+                    for(int j=0;j<colun-1;j++)
+                    {
+                        System.out.print(".");
+                    }
+                    System.out.print("#");
+                }
+                else
+                {
+                    System.out.print("#");
+                    for(int j=0;j<colun-1;j++)
+                    {
+                        System.out.print(".");
+                    }
 
+                }
+                right = !right;
             }
             System.out.println();
-            right = false;
+
+
+
 
         }
     }
