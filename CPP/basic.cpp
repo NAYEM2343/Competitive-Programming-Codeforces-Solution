b646af65043a16576d07030e8e6319ff4d0d9caf
NAYEM2343 <nayemsheikh822@gmail.com>
2026-09-05 23:29:50 +0600

    Added

--- CPP/basic.cpp
+++ CPP/basic.cpp
@@ -25,27 +25,15 @@ int main()
     vec[n-1] +=1;
 
     if(vec[n-1]==10)
-
     {
-
         vec[n-1] = 1;
 
         vec.push_back(0);
-
     }
-
-    for(int i=0;i<n;i++)
-
+    for(int i=0;i<vec.size();i++)
     {
-
         cout<<vec[i]<<endl;
-
     }
-
-
-
-
-
     return 0;
 
 }
