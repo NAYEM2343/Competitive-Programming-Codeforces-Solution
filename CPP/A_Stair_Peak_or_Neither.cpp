3c6e2ab2ed3af47469ee165866e2f40695bdcde3
NAYEM2343 <nayemsheikh822@gmail.com>
2026-09-19 12:34:48 +0600

    ADDED

---
+++ CPP/A_Stair_Peak_or_Neither.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a,b,c;cin>>a>>b>>c;
+    if(a<b && b<c)
+    {
+        cout<<"STAIR"<<endl;
+    }
+    else if(a<b && b>c)
+    {
+        cout<<"PEAK"<<endl;
+    }
+    else
+    {
+        cout<<"NONE"<<endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
