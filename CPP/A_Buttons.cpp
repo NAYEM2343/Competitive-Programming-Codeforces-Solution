93ce7e01b27abcfdf8ab36368021ee87bbf85100
NAYEM2343 <nayemsheikh822@gmail.com>
2026-09-25 19:38:12 +0600

    ADDE

---
+++ CPP/A_Buttons.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a,b,c;cin>>a>>b>>c;
+    if(c%2==0)
+    {
+        if(a>b)
+        {
+            cout<<"First"<<endl;
+        }
+        else
+        {
+            cout<<"Second"<<endl;
+        }
+    }
+    else
+    {
+        if(b>a)
+        {
+            cout<<"Second"<<endl;
+        }
+        else
+        {
+            cout<<"First"<<endl;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
